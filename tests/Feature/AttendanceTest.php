bb751d5cd94dad3b4285bd5cf3acdda90a1ee7d0
Ibrahim Ahmed <ibrahim21383@gmail.com>
2018-10-03 19:11:20 +0200

    test for init_state in fresh user

--- tests/Feature/AttendanceTest.php
+++ tests/Feature/AttendanceTest.php
@@ -6,7 +6,6 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Artisan;
-use App\User;
 
 class AttendanceTest extends TestCase
 {
@@ -14,13 +13,48 @@ class AttendanceTest extends TestCase
 
    public function test_init_state_with_fresh_user()
    {
+       //create initial settings and dummy testing users data
        Artisan::call('seed:settings');
        Artisan::call('seed:users');
-       $this->assertEquals(11,User::all()->count());
+
+       //enable fake authentication
        config(['app.enable_fake_login' => true]);
        config(['app.fake_logged_user' => 11]);
+
+       //make the test request to init_state
        $response = $this->json('GET','init_state');
        $content = json_decode($response->content(), true);
-       dd($response->status());
+
+       //test that response succeeded
+       $this->assertEquals(200,$response->status());
+       $this->assertCount(5,$content);
+
+        //test the flags field
+       $flags = $content['flags'];
+       $actualFlags = app('settings')->getFlags();
+       $this->assertCount(count($actualFlags),$flags);
+       foreach ($flags as $flag) {
+           $this->assertTrue(isset($actualFlags[$flag['type']]));
+           $this->assertEquals($actualFlags[$flag['type']] * 60 * 60,$flag['limitSeconds']);
+           $this->assertEquals($actualFlags[$flag['type']] * 60 * 60,$flag['remainingSeconds']);
+           $this->assertTrue(!$flag['inUse']);
+       }
+
+       //test the work time signs field
+       $this->assertCount(0,$content['workTimeSigns']);
+
+       //test the status field
+       $this->assertEquals('off',$content['status']);
+
+       //test the today time field
+       $todayTime = $content['today_time'];
+       $this->assertEquals(0,$todayTime['seconds']);
+       $this->assertEquals('',$todayTime['workStatus']);
+
+       //test the month report
+       $monthReport = $content['month_report'];
+       $this->assertEquals(0,$monthReport['actual']['seconds']);
+       $this->assertEquals($monthReport['ideal']['seconds'],$monthReport['diff']['seconds']);
+       $this->assertEquals('less',$monthReport['diff']['type']);
    }
 }
